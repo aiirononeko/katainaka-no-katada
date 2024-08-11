9183f8dec0d5eb151af7023c817c6a9d00dd6463
aiirononeko <aiirononeko@gmail.com>
2024-08-12 01:04:12 +0900

    remix + bun + shadcnui project init

---
+++ app/routes/_index.tsx
@@ -0,0 +1,18 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="font-sans p-4">
+      <h1 className="text-3xl">Welcome to Remix</h1>
+      <Button>ボタン</Button>
+    </div>
+  );
+}
