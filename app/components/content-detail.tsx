8ad9999e5d8bec527aa61e09f3841897954e8a01
aiirononeko <aiirononeko@gmail.com>
2024-08-17 02:19:49 +0900

    スマホ表示時のスタイルを微修正

--- app/components/content-detail.tsx
+++ app/components/content-detail.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@remix-run/react'
 import parse from 'html-react-parser'
 import { Badge } from '~/components/ui/badge'
 
@@ -11,7 +10,7 @@ interface Props {
 export const ContentDetail = ({ content }: Props) => {
   return (
     <div className='container mx-auto grid grid-cols-4 gap-8 w-full max-w-[1120px]'>
-      <div className='col-span-4 border rounded container py-10 lg:col-span-3'>
+      <div className='col-span-4 border rounded py-6 px-4 sm:px-10 md:py-10 lg:col-span-3'>
         <div className='space-x-2'>
           {content.tags.map((tag) => (
             <Badge key={tag.id} variant='outline' className='h-8 space-x-1'>
@@ -27,6 +26,9 @@ export const ContentDetail = ({ content }: Props) => {
         <div className='h-80 border rounded flex justify-center items-center'>
           <p className='text-muted-foreground'>目次準備中...</p>
         </div>
+      </div>
+      <div className='lg:hidden col-span-4'>
+        <Introduce />
       </div>
     </div>
   )
