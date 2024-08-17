abb7a0d8697437675a7b1f9de8f967203a106e04
aiirononeko <aiirononeko@gmail.com>
2024-08-17 18:41:03 +0900

    sitemapを自動生成できるように修正

--- app/routes/sitemap[.]xml.tsx
+++ app/routes/sitemap[.]xml.tsx
@@ -1,4 +1,26 @@
-export const loader = () => {
+import { LoaderFunctionArgs } from '@remix-run/cloudflare'
+import { createClient } from 'microcms-js-sdk'
+
+export const loader = async ({ context }: LoaderFunctionArgs) => {
+  const client = createClient({
+    serviceDomain: context.cloudflare.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: context.cloudflare.env.MICROCMS_API_KEY,
+  })
+
+  const { contents: blogs } = await client.getList<Blog>({
+    endpoint: 'blogs',
+    queries: {
+      fields: ['id', 'updatedAt', 'category.slug'],
+    },
+  })
+
+  const { contents: categories } = await client.getList<Category>({
+    endpoint: 'categories',
+    queries: {
+      fields: ['slug', 'updatedAt'],
+    },
+  })
+
   const content = `
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
@@ -7,12 +29,26 @@ export const loader = () => {
         <priority>1.0</priority>
         <changefreq>daily</changefreq>
       </url>
+  ${categories.map((category) => {
+    return `
+          <url>
+            <loc>https://www.kissa-katada.com/${category.slug}</loc>
+            <lastmod>${category.updatedAt}</lastmod>
+            <priority>1.0</priority>
+            <changefreq>daily</changefreq>
+          </url>
+        `
+  })}
+  ${blogs.map((blog) => {
+    return `
       <url>
-        <loc>https://www.kissa-katada.com/daily</loc>
-        <lastmod>2024-08-17</lastmod>
+        <loc>https://www.kissa-katada.com/${blog.category.slug}/${blog.id}</loc>
+        <lastmod>${blog.updatedAt}</lastmod>
         <priority>1.0</priority>
         <changefreq>daily</changefreq>
       </url>
+    `
+  })}
     </urlset>
     `
 
