709ae0f8169141a09328bfbcfb5379b4efadb59c
aiirononeko <aiirononeko@gmail.com>
2024-08-23 02:16:27 +0900

    headerのz-index指定

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ import { Link } from '@remix-run/react'
 
 export const Header = () => {
   return (
-    <header className='h-24 md:h-28 flex items-center sticky top-0 bg-white md:static'>
+    <header className='h-24 md:h-28 flex items-center sticky top-0 bg-white md:static z-10'>
       <div className='container mx-auto max-w-[1120px] relative'>
         <h1 className='font-bold text-xl md:text-3xl text-center'>
           <Link to='/'>キッサ カタダ</Link>
