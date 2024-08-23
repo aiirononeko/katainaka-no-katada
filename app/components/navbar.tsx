f08256708cf74e41b757195c573689cb9291f6a9
aiirononeko <aiirononeko@gmail.com>
2024-08-23 22:43:07 +0900

    hydration error解消

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -29,7 +29,7 @@ export const Navbar = () => {
       <ul className='flex justify-center font-semibold text-xs md:text-sm'>
         {NAV_ITEMS.map((item) => (
           <li
-            key={`${item.name}_${Math.random()}`}
+            key={`${item.name}_${item.path}`}
             className='h-12 w-32 flex items-center justify-center tracking-wide sm:tracking-widest hover:bg-muted hover:underline'
           >
             <NavLink
